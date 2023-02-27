59a217e8e987b36fd5c1ebf828bd22ab1d0b5cc2
Ahmed Elkoumey <ahmedelkoumey1997@gmail.com>
2023-02-27 22:06:25 +0200

    last edit

--- app/Config/config.php
+++ app/Config/config.php
@@ -5,7 +5,7 @@ define("BURL",'http://localhost/crud_mvc/public/');
 
 
 //db config 
-define("HOST","localhost");
-define("USER","root");
-define("PASSWORD",NULL);
-define("DBNAME","mvc");
+define("HOST","localhost:3306");
+define("USER","id20359004_root");
+define("PASSWORD","V/_7$*dp?*tj3zuy");
+define("DBNAME","id20359004_mvc");
